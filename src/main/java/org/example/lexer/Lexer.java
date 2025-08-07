e51935f455d3657db6d72e961d7fcb0116310c10
celina <cariasduval@mail.austral.edu.ar>
2025-08-06 21:06:03 -0300

    part one: Lexer base

---
+++ src/main/java/org/example/lexer/Lexer.java
@@ -0,0 +1,26 @@
+package org.example.lexer;
+
+import org.example.common.tokens.Tokens;
+
+import java.util.List;
+
+public class Lexer {
+    public List<Tokens> lex(String string){
+        //funcion slipt
+        //funcion identify el type
+        //funcion tokenizer
+        //poner el token en la lista
+    }
+
+    //nivel de dificultad = BAJO
+    public String split(String string){
+        return string;
+    }
+
+    //nivel de dificultad = ALTO
+    public String identifyToken(String string){}
+
+    //nivel de dificultad = MEDIO
+    public Tokens tokenizer(String string){}
+
+}
