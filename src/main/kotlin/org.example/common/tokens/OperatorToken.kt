73c6bd36bf443eb11cd971d072ccc9cfc8e280a8
celina <cariasduval@mail.austral.edu.ar>
2025-08-09 15:17:57 -0300

    PunctuationSymbolsToken corrections

--- src/main/kotlin/org.example/common/tokens/OperatorToken.kt
+++ src/main/kotlin/org.example/common/tokens/OperatorToken.kt
@@ -1,5 +1,8 @@
 package org.example.common.tokens
 
+import org.example.common.Range
+
 data class OperatorToken(
-    var type: Operator
+    var type: Operator,
+    val range: Range
 ) : Token
--- src/main/kotlin/org.example/common/tokens/PunctuationSymbolsToken.kt
+++ src/main/kotlin/org.example/common/tokens/PunctuationSymbolsToken.kt
@@ -1,5 +1,8 @@
 package org.example.common.tokens
 
+import org.example.common.Range
+
 data class PunctuationSymbolsToken(
-    val type: Punctuation
+    val type: Punctuation,
+    val range: Range
 ): Token
