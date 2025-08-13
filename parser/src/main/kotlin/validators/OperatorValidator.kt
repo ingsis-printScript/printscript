03a452b4d68538dcaacd50b932bba954cc7f8f3c
skeczeli <skeczeli@gmail.com>
2025-08-13 20:25:38 -0300

    Algo de la estructura para ExpressionParser

--- parser/src/main/kotlin/validators/OperatorValidator.kt
+++ parser/src/main/kotlin/validators/OperatorValidator.kt
@@ -3,20 +3,17 @@ package org.example.parser.validators
 import org.example.common.tokens.Token
 import org.example.common.tokens.enums.Operator
 
-class OperatorValidator(private val expected: Operator) : TokenValidator {
-    private val expectedSymbol = expected.symbol
-
+class OperatorValidator() : TokenValidator {
     override fun validate(statement: List<Token>, position: Int): ValidationResult {
         return when (val token: Token = statement[position]) {
             is OperatorToken -> {
-                if (token.kind.symbol == expectedSymbol) {
-                    ValidationResult.Success(1)
-                } else {
-                    ValidationResult.Error("Expected '${expectedSymbol}', found '${token.kind.symbol}'", position)
-                }
+                ValidationResult.Success(1)
             }
-            else -> ValidationResult.Error("Expected operator '${expectedSymbol}'," +
-                    " found ${token::class.simpleName}", position)
+            else -> {
+                val operations = Operator.entries.map { entry -> entry.symbol }
+                ValidationResult.Error("Expected operator '$operations'," +
+                        " found ${token::class.simpleName}", position)
+            }
         }
     }
 
