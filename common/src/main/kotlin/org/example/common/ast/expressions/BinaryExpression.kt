02944ea1451826c6cf5ea97941f1ba67f0b31423
skeczeli <skeczeli@gmail.com>
2025-08-23 19:05:52 -0300

    ast nodes are now data classes

--- common/src/main/kotlin/org/example/common/ast/expressions/BinaryExpression.kt
+++ common/src/main/kotlin/org/example/common/ast/expressions/BinaryExpression.kt
@@ -3,7 +3,7 @@ package org.example.common.ast.expressions
 import org.example.common.Position
 import org.example.common.enums.Operator
 
-class BinaryExpression(
+data class BinaryExpression(
     val left: Expression,
     val operator: Operator,
     val right: Expression,
--- common/src/main/kotlin/org/example/common/ast/statements/FunctionCall.kt
+++ common/src/main/kotlin/org/example/common/ast/statements/FunctionCall.kt
@@ -4,7 +4,7 @@ import org.example.common.Range
 import org.example.common.ast.expressions.Expression
 import org.example.common.ast.expressions.IdentifierExpression
 
-class FunctionCall(
+data class FunctionCall(
     val identifier: IdentifierExpression,
     val value: Expression,
     override val range: Range
