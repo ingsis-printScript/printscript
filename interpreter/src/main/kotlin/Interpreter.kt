85bc22c3c6a3adc7762cafc52ef3693123ca1659
celina <cariasduval@mail.austral.edu.ar>
2025-09-02 00:21:32 -0300

    interpreter

--- interpreter/src/main/kotlin/Interpreter.kt
+++ interpreter/src/main/kotlin/Interpreter.kt
@@ -1,16 +1,26 @@
 package org.example.interpreter
 
 import org.example.common.PrintScriptIterator
+import org.example.ast.ASTNode
 import org.example.interpreter.result.Results
 
+class Interpreter(
+    private val iterator: PrintScriptIterator<ASTNode>,
+    private val validator: Validator,
+    private val executor: Executor
+) : PrintScriptIterator<Results> {
 
-class Interpreter : PrintScriptIterator<Results> {
     override fun hasNext(): Boolean {
-        TODO("Not yet implemented")
+        return iterator.hasNext()
     }
 
     override fun getNext(): Results {
-        TODO("Not yet implemented")
-    }
+        val node = iterator.getNext()
 
+        // Primero validamos el nodo
+        validator.visit(node)
+
+        // Luego ejecutamos el nodo
+        return executor.visit(node)
+    }
 }
