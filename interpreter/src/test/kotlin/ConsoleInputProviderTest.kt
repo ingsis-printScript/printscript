9c4b0bbda7a97f62930fdb85fd3062f5bde02094
celina <cariasduval@mail.austral.edu.ar>
2025-09-14 16:30:07 -0300

    Interpreter Handlers Tests and Fixes

---
+++ interpreter/src/test/kotlin/ConsoleInputProviderTest.kt
@@ -0,0 +1,33 @@
+package org.example.interpreter.input
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import java.io.ByteArrayInputStream
+import java.io.InputStream
+
+class ConsoleInputProviderTest {
+
+    @Test
+    fun `should read input from console`() {
+        val simulatedInput = "Hola Mundo\n"
+        val inputStream: InputStream = ByteArrayInputStream(simulatedInput.toByteArray())
+        System.setIn(inputStream)
+
+        val provider = ConsoleInputProvider()
+        val result = provider.readInput("Escriba algo")
+
+        assertEquals("Hola Mundo", result)
+    }
+
+    @Test
+    fun `should return empty string when input is empty`() {
+        val simulatedInput = "\n"
+        val inputStream: InputStream = ByteArrayInputStream(simulatedInput.toByteArray())
+        System.setIn(inputStream)
+
+        val provider = ConsoleInputProvider()
+        val result = provider.readInput("Ingrese algo")
+
+        assertEquals("", result)
+    }
+}
---
+++ interpreter/src/test/kotlin/handlers/VariableAssignerHandlerTest.kt
@@ -0,0 +1,107 @@
+package handlers
+
+import org.example.ast.expressions.OptionalExpression
+import org.example.ast.expressions.SymbolExpression
+import org.example.ast.statements.VariableAssigner
+import org.example.ast.statements.VariableImmutableDeclarator
+import org.example.common.Position
+import org.example.common.Range
+import org.example.common.enums.Type
+import org.example.interpreter.Executor
+import org.example.interpreter.Validator
+import org.example.interpreter.handlers.ASTNodeHandler
+import org.example.interpreter.handlers.VariableAssignerHandler
+import org.example.interpreter.handlers.VariableImmutableDeclaratorHandler
+import org.example.interpreter.output.ErrorHandler
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class VariableAssignerHandlerTest {
+
+    private val fakeErrorHandler = object : org.example.interpreter.output.ErrorHandler {
+        val errors = mutableListOf<String>()
+        override fun handleError(message: String) {
+            errors.add(message)
+        }
+    }
+
+    private val handlers: Map<Class<out org.example.ast.ASTNode>, ASTNodeHandler<*>> = mapOf(
+        VariableAssigner::class.java to VariableAssignerHandler(),
+        SymbolExpression::class.java to object : ASTNodeHandler<SymbolExpression> {
+            override fun handleExecution(node: SymbolExpression, executor: org.example.interpreter.Executor) {}
+            override fun handleValidation(node: SymbolExpression, validator: Validator) {
+                val type = when (node.value) {
+                    "5", "10" -> Type.NUMBER
+                    "true", "false" -> Type.BOOLEAN
+                    else -> Type.NUMBER
+                }
+                validator.pushLiteral(type)
+            }
+        }
+    )
+
+
+    private val validator = Validator(handlers, fakeErrorHandler)
+
+    @Test
+    fun `should assign value to existing variable`() {
+        val declarator = VariableImmutableDeclarator(
+            SymbolExpression("x", Position(0,0)),
+            Type.NUMBER,
+            Range(Position(0,0), Position(0,1)),
+            OptionalExpression.HasExpression(SymbolExpression("5", Position(0,0)))
+        )
+        val declaratorHandler = VariableImmutableDeclaratorHandler()
+        declaratorHandler.handleValidation(declarator, validator)
+
+        val assigner = VariableAssigner(
+            SymbolExpression("x", Position(1,0)),
+            OptionalExpression.HasExpression(SymbolExpression("10", Position(1,0))),
+            Range(Position(1,0), Position(1,1))
+        )
+        val handler = handlers[VariableAssigner::class.java] as VariableAssignerHandler
+        handler.handleValidation(assigner, validator)
+
+        val type = validator.popLiteral()
+        assertEquals(Type.NUMBER, type)
+    }
+
+
+    @Test
+    fun `should throw error if type mismatch`() {
+        val declarator = VariableImmutableDeclarator(
+            SymbolExpression("x", Position(0, 0)),
+            Type.NUMBER,
+            Range(Position(0, 0), Position(0, 1)),
+            OptionalExpression.HasExpression(SymbolExpression("5", Position(0, 0)))
+        )
+        val declaratorHandler = VariableImmutableDeclaratorHandler()
+        declaratorHandler.handleValidation(declarator, validator)
+
+        val assigner = VariableAssigner(
+            SymbolExpression("x", Position(1,0)),
+            OptionalExpression.HasExpression(SymbolExpression("true", Position(1,0))), // boolean en vez de number
+            Range(Position(1,0), Position(1,1))
+        )
+        val handler = handlers[VariableAssigner::class.java] as VariableAssignerHandler
+        val exception = assertThrows<RuntimeException> {
+            handler.handleValidation(assigner, validator)
+        }
+        assertTrue(exception.message!!.contains("Type mismatch"))
+    }
+
+    @Test
+    fun `should throw error if no expression`() {
+        val assigner = VariableAssigner(
+            SymbolExpression("x", Position(0,0)),
+            OptionalExpression.NoExpression,
+            Range(Position(0,0), Position(0,1))
+        )
+        val handler = handlers[VariableAssigner::class.java] as VariableAssignerHandler
+        val exception = assertThrows<RuntimeException> {
+            handler.handleValidation(assigner, validator)
+        }
+        assertTrue(exception.message!!.contains("Missing expression"))
+    }
+}
