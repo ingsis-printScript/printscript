0192373cedb69a78ac15ee28254f5a605af8296c
skeczeli <skeczeli@gmail.com>
2025-09-16 14:48:02 -0300

    n4

--- formatter/src/main/kotlin/org/example/formatter/providers/FormatterVersionProvider.kt
+++ formatter/src/main/kotlin/org/example/formatter/providers/FormatterVersionProvider.kt
@@ -5,8 +5,8 @@ import org.example.formatter.Ruler
 class FormatterVersionProvider {
     fun with(version: String): FormatterProvider {
         return when (version) {
-            "1.0" -> FormatterProvider10(Ruler(mapOf())) // TODO: rule map
-            "1.1" -> FormatterProvider11(Ruler(mapOf())) // TODO: rule map
+            "1.0" -> FormatterProvider10()
+            "1.1" -> FormatterProvider11()
             else -> throw IllegalArgumentException("Unsupported version: $version")
         }
     }
